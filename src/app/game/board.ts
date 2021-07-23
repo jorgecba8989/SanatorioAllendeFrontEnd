10f893813fe8e907c03e72ae9da5feb71abe8308
jorgecba8989 <georgecba89@gmail.com>
2021-07-23 16:41:47 -0300

    Implementacion de la asignacion y cantidad de minas que habra en la tabla

--- src/app/game/board.ts
+++ src/app/game/board.ts
@@ -1,16 +1,39 @@
 import { Cell } from './cell';
 
+const PEERS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
 export class Board {
     cells: Cell[][] = [];
 
+    remainingCells = 0;
+    mineCount = 0;
 
     constructor(size: number, mines: number) {
         for (let y = 0; y < size; y++) {
-          this.cells[y] = [];
-          for (let x = 0; x < size; x++) {
-            this.cells[y][x] = new Cell(y, x);
-          }
+            this.cells[y] = [];
+            for (let x = 0; x < size; x++) {
+                this.cells[y][x] = new Cell(y, x);
+            }
         }
-    }
 
+        for (let y = 0; y < size; y++) {
+            for (let x = 0; x < size; x++) {
+              let adjacentMines = 0;
+              for (const peer of PEERS) {
+                if ( this.cells[y + peer[0]] && this.cells[y + peer[0]][x + peer[1]] && this.cells[y + peer[0]][x + peer[1]].mine) {
+                  adjacentMines++;
+                }
+              }
+              this.cells[y][x].proximityMines = adjacentMines;
+              if (this.cells[y][x].mine) { this.mineCount++;}
+            }
+          }
+          this.remainingCells = size * size - this.mineCount;
+        }
+      
+        getRandomCell(): Cell {
+          const y = Math.floor(Math.random() * this.cells.length);
+          const x = Math.floor(Math.random() * this.cells[y].length);
+          return this.cells[y][x];
+        }
 }  

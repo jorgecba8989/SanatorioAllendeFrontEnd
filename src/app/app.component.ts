36f9acbc998e66410987902c49689348d854164b
jorgecba8989 <georgecba89@gmail.com>
2021-07-23 16:23:35 -0300

    Implementacion de la clase board y visualizacion de la tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { Board } from './game/board';
+import { Cell } from './game/cell';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Buscaminas';
   time: number = 0;
   interval: any;
+  board!: Board;
+
+
+  constructor( ) {
+    this.board = new Board(5, 5);
+  }
 
   startTimer() {
     this.interval = setInterval(() => {

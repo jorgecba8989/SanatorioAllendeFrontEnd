25ee8c1416123c96c225987eadcd72666d0b2530
jorgecba8989 <georgecba89@gmail.com>
2021-07-23 15:33:59 -0300

    Creacion del proyecto e implementacion del starTimer y pauseTimer

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Buscaminas';
+  time: number = 0;
+  interval: any;
+
+  startTimer() {
+    this.interval = setInterval(() => {
+        this.time++;
+    },1000)
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+  
+}

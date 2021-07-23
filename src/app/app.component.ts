9afac5d1778e08f1b633e9df75600825f5a02ed5
jorgecba8989 <georgecba89@gmail.com>
2021-07-23 15:35:46 -0300

    Creacion del proyecto e implementacion del starTimer y pauseTimer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,5 +19,4 @@ export class AppComponent {
   pauseTimer() {
     clearInterval(this.interval);
   }
-  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClockComponent } from './clock/clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ClockComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
